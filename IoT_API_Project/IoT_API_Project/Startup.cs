2826208231e221c0c5810aec32c99e8b25d4455b
SillyRaven <88430351+SillyRaven@users.noreply.github.com>
2022-09-06 21:09:46 +0200

    Trying to figure stuff out(failing)

--- IoT_API_Project/IoT_API_Project/Startup.cs
+++ IoT_API_Project/IoT_API_Project/Startup.cs
@@ -17,12 +17,19 @@ builder.Services.AddDbContext<DeviceContext>(opt =>
 builder.Services.AddDbContext<ZoneContext>(opt =>
     opt.UseInMemoryDatabase("Zone"));
 
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new() { Title = "InternetOfThings", Version = "v1" });
+//});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+   // app.UseSwagger();
+   // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
 
 app.UseHttpsRedirection();
