efe591fc25bb15c67968631c3b846f9b8454c846
SillyRaven <88430351+SillyRaven@users.noreply.github.com>
2022-09-06 18:49:46 +0200

    Models added

---
+++ IoT_API_Project/IoT_API_Project/Models/CategoriesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project2_IoT_Management.Models
+{
+        public class CategoriesContext : DbContext
+        {
+            public CategoriesContext(DbContextOptions<CategoriesContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Categories> categories { get; set; } = null!;
+        }
+}
