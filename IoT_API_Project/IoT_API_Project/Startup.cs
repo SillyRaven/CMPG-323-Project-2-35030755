3d4af6a776221a44f57d21e5a14253026832ca5b
SillyRaven <88430351+SillyRaven@users.noreply.github.com>
2022-09-07 17:35:21 +0200

    updates

--- IoT_API_Project/IoT_API_Project/Startup.cs
+++ IoT_API_Project/IoT_API_Project/Startup.cs
@@ -17,10 +17,10 @@ builder.Services.AddDbContext<DeviceContext>(opt =>
 builder.Services.AddDbContext<ZoneContext>(opt =>
     opt.UseInMemoryDatabase("Zone"));
 
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new() { Title = "InternetOfThings", Version = "v1" });
-//});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "InternetOfThings", Version = "v1" });
+});
 
 var app = builder.Build();
 
