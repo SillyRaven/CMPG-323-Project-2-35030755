e623a8258890b4b8a34c425f6d7ea78b86d27a04
Peach G <88430351+SillyRaven@users.noreply.github.com>
2022-09-07 15:33:48 +0200

    Swagger changes

--- IoT_API_Project/IoT_API_Project/Startup.cs
+++ IoT_API_Project/IoT_API_Project/Startup.cs
@@ -28,8 +28,8 @@ var app = builder.Build();
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-   // app.UseSwagger();
-   // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
 
 app.UseHttpsRedirection();
