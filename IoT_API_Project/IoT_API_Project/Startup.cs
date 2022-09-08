2643d747db2c67c42eb4f9226f701eff126aedd5
SillyRaven <88430351+SillyRaven@users.noreply.github.com>
2022-09-08 12:35:05 +0200

    Con

--- IoT_API_Project/IoT_API_Project/Startup.cs
+++ IoT_API_Project/IoT_API_Project/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 IServiceCollection services = builder.Services;
