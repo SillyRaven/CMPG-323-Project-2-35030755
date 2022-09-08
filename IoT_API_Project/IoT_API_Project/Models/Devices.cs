25a7efcf1d0e0c2b727130e7fcd1045306c21997
SillyRaven <88430351+SillyRaven@users.noreply.github.com>
2022-09-08 11:54:52 +0200

    Config changes

--- IoT_API_Project/IoT_API_Project/Models/Devices.cs
+++ IoT_API_Project/IoT_API_Project/Models/Devices.cs
@@ -6,7 +6,7 @@ namespace Project2_IoT_Management.Models
     {
         [Key]public long DeviceId { get; set; }
         public long ZoneID { get; set; }
-        public bool CategoryID { get; set; }
+        public long CategoryID { get; set; }
         public string? DeviceName { get; set; }
         public string? Status { get; set; }
         public DateTime DateCreated { get; set; }
